1053826a72974174975d390373d7f6036831d30a
83925 <83925@LHP>
2025-05-18 23:26:09 +0800

    提交代码

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace Webapi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","1"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","2"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
